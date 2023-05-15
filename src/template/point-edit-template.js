1bee4b7c030bf45577f369a6f50776a349858aed
DenisNilov <denis-nilov@yandex.ru>
2023-05-15 18:24:12 +0300

    2.16 Доработка шаблона точки редактирования пункта назначения

--- src/template/point-edit-template.js
+++ src/template/point-edit-template.js
@@ -1,12 +1,10 @@
 import { CITIES } from '../mock/const.js';
 import dayjs from 'dayjs';
 import { createOffersList } from './point-edit-offers-template.js';
-
+import { createDistinationsList } from './point-edit-distination-template.js';
 
 function createPointEditTemplate({ point, pointDestination, pointOffers }) {
   const { basePrice, dateFrom, dateTo, destination, type } = point;
-
-
   return (/* html */
     `<form class="event event--edit" action="#" method="post">
       <header class="event__header">
@@ -106,24 +104,11 @@ function createPointEditTemplate({ point, pointDestination, pointOffers }) {
         <section class="event__section  event__section--offers">
           <h3 class="event__section-title  event__section-title--offers">Offers</h3>
           <div class="event__available-offers">
-          ${createOffersList(pointOffers, {type})}
+          ${createOffersList(pointOffers, { type })}
           </div>
         </section>
+        ${createDistinationsList(pointDestination, { destination })}
 
-        <section class="event__section  event__section--destination">
-          <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-          <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
-
-          <div class="event__photos-container">
-            <div class="event__photos-tape">
-              <img class="event__photo" src="img/photos/1.jpg" alt="Event photo">
-              <img class="event__photo" src="img/photos/2.jpg" alt="Event photo">
-              <img class="event__photo" src="img/photos/3.jpg" alt="Event photo">
-              <img class="event__photo" src="img/photos/4.jpg" alt="Event photo">
-              <img class="event__photo" src="img/photos/5.jpg" alt="Event photo">
-            </div>
-          </div>
-        </section>
       </section>
     </form>`
   );
