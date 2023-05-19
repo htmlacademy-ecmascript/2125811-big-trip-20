023c18afaab6500b0e78c0057a18dd756347ce18
DenisNilov <denis-nilov@yandex.ru>
2023-05-19 23:35:48 +0300

    Отрефактрит разметку формы

--- src/template/point-edit-template.js
+++ src/template/point-edit-template.js
@@ -99,7 +99,10 @@ function createPointEditTemplate({ point, pointDestination, pointOffers }) {
         </div>
 
         <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-        <button class="event__reset-btn" type="reset">Cancel</button>
+        <button class="event__reset-btn" type="reset">Delete</button>
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
       </header>
       <section class="event__details">
         <section class="event__section  event__section--offers">
