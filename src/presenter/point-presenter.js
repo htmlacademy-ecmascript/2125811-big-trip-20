f536b33d013a91ff5cbdb4a4ac7545fe23f95dff
DenisNilov <denis-nilov@yandex.ru>
2023-05-27 21:37:22 +0300

    5.3 Добавит возможность переиспользования

--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -1,6 +1,6 @@
 import PointEditView from '../view/point-edit-view.js';
 import PointView from '../view/point-view.js';
-import { render, replace } from '../framework/render.js';
+import { render, replace, remove } from '../framework/render.js';
 
 export default class PointPresenter {
 
@@ -19,6 +19,8 @@ export default class PointPresenter {
     this.#destinationsModel = destinationsModel;
     this.#offersModel = offersModel;
     this.#point = point;
+    const prevPointComponent = this.#pointComponent;
+    const prevPointEditComponent = this.#pointEditComponent;
 
 
     this.#pointComponent = new PointView({
@@ -35,8 +37,27 @@ export default class PointPresenter {
       onFormSubmit: this.#handleFormSubmit,
       onResetClick: this.#handleFormSubmit
     });
+    if (prevPointComponent === null || prevPointEditComponent === null) {
+      render(this.#pointComponent, this.#pointListContainer);
+      return;
+    }
 
-    render(this.#pointComponent, this.#pointListContainer);
+    if (this.#pointListContainer.contains(prevPointComponent.element)) {
+      replace(this.#pointComponent, prevPointComponent);
+    }
+
+    if (this.#pointListContainer.contains(prevPointEditComponent.element)) {
+      replace(this.#pointEditComponent, prevPointEditComponent);
+    }
+
+    remove(prevPointComponent);
+    remove(prevPointEditComponent);
+
+  }
+
+  destroy() {
+    remove(this.#pointComponent);
+    remove(this.#pointEditComponent);
   }
 
   #escKeyDownHandler = (evt) => {
