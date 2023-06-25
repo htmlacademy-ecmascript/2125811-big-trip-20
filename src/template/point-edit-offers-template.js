8ec96ecfcb02e3950e58598ff47e9ac0a14a1ed5
DenisNilov <denis-nilov@yandex.ru>
2023-06-25 18:14:39 +0300

    Отрифакторит шаблон отображение доп.предлжений

--- src/template/point-edit-offers-template.js
+++ src/template/point-edit-offers-template.js
@@ -1,13 +1,14 @@
 function createOffersList(point, pointOffers, { type }) {
   return (
-    pointOffers.find((el) => el.type === type).offers.map((offer, index) => /* html */
+    pointOffers.find((el) => el.type === type).offers.map((offer) => /* html */
       `<div class="event__offer-selector">
       <input class="event__offer-checkbox  visually-hidden"
-      id="event-offer-luggage-${index}" type="checkbox"
-      name="event-offer-luggage"
+      id="event-offer-luggage-${offer.id}" type="checkbox"
+      name="event-offer-${offer.title}"
+      data-offer-id="${offer.id}"
       ${point.offers.find((id) => id === offer.id) === offer.id ? 'checked' : ''}
       >
-      <label class="event__offer-label" for="event-offer-luggage-${index}">
+      <label class="event__offer-label" for="event-offer-luggage-${offer.id}">
         <span class="event__offer-title">${offer.title}</span>
           &plus;&euro;&nbsp;
         <span class="event__offer-price">${offer.price}</span>
